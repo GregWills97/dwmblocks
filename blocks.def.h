aaa9e1a288b1372ba9d34691c7e52707f5bd7115
Gregory Williams <gregwills85@gmail.com>
2023-04-06 22:31:24 -0600

    Update to status bar blocks

--- blocks.def.h
+++ blocks.def.h
@@ -1,9 +1,14 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
-
-	{"", "date '+%b %d (%a) %I:%M%p'",					5,		0},
+	{"",	"/home/greg/Documents/gde/dwmblocks/gde-sbkernel",		0,		1},
+	{"",	"/home/greg/Documents/gde/dwmblocks/gde-sbtemps",		3,		2},
+	{"",	"/home/greg/Documents/gde/dwmblocks/gde-sbmemory",		3,		3},
+	{"",	"/home/greg/Documents/gde/dwmblocks/gde-sbvolume",		0,		4},
+	{"",	"/home/greg/Documents/gde/dwmblocks/gde-sbbattery",		5,		5},
+	{"",	"/home/greg/Documents/gde/dwmblocks/gde-sbclock day",	3600,	6},
+	{"",	"/home/greg/Documents/gde/dwmblocks/gde-sbclock time",	60,		7},
+	{"",	"/home/greg/Documents/gde/dwmblocks/gde-sbinternet",	5,		8},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
